7d184329c0e1199678186a3084c9b1aed459f3bd
Mohit-kumar-shaw <69642308+Mohit-kumar-shaw@users.noreply.github.com>
2020-10-08 22:31:20 +0530

    this is simple way to swap ,just take  swap variable .

--- swapping.1.c
+++ swapping.1.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 main()
 {
-  int a,b; 
+  int a,b,swap=0; 
   printf("enter first number");
   scanf("%d",&a);
   printf("enter second number");
   scanf("%d",&b);
-  a=a+b;
-  b=a-b;
-  a=a-b;
+  swap=a;
+  a=b;
+  b=swap;
   printf("value after swapping");
   printf("value of A %d,a");
   printf("value of B %d,b");
